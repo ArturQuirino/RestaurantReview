24367ba775f55d0456a5347cb39786538e3b6265
Artur Quirino <artur.quirino@mrv.com.br>
2018-12-21 10:57:02 -0200

    change header level

--- js/main.js
+++ js/main.js
@@ -181,7 +181,7 @@ createRestaurantHTML = (restaurant) => {
   image.alt = 'Picture of the restaurant ' + restaurant.name;
   li.append(image);
 
-  const name = document.createElement('h1');
+  const name = document.createElement('h2');
   name.innerHTML = restaurant.name;
   li.append(name);
 
